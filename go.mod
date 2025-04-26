9aacb2e07c905a16de25a8b77a21f9084745b75d
mtchuikov <mtchuikov@yandex.ru>
2025-04-26 17:42:12 +0300

    fix oapi files

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/PiskarevSA/minimarket
 
 go 1.24.0
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/oapi-codegen/runtime v1.1.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/PiskarevSA/minimarket/internal/gen/oapi"
+	"github.com/go-chi/chi/v5"
+)
+
+var _ oapi.ServerInterface = (*Handlers)(nil)
+
+type Handlers struct{}
+
+func NewHandlers() *Handlers {
+	return &Handlers{}
+}
+
+func (h *Handlers) PostApiUserRegister(rw http.ResponseWriter, req *http.Request) {
+	rw.Write([]byte("hello world!"))
+}
+
+func main() {
+	rootCtx := context.Background()
+	signals := []os.Signal{syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL}
+
+	stopCtx, stop := signal.NotifyContext(rootCtx, signals...)
+	defer stop()
+
+	handlers := NewHandlers()
+	router := chi.NewRouter()
+
+	h := oapi.HandlerFromMux(handlers, router)
+
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: h,
+	}
+
+	log.Println("listening server on 127.0.0.1:8080...")
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal("failed to listen server")
+		}
+	}()
+
+	<-stopCtx.Done()
+}
