d5ce3b0a31203fed47de31b0c79af8930300b5ea
Semen Piskarev <piskarevsa@mail.ru>
2025-05-12 00:03:06 +0300

    auth test: add TestUserLogInSuccess() into userLogInSuite

--- microservices/auth/internal/usecases/user_login_test.go
+++ microservices/auth/internal/usecases/user_login_test.go
@@ -1,10 +1,20 @@
 package usecases
 
 import (
+	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/PiskarevSA/minimarket/microservices/auth/internal/storage"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type userLogInSuite struct {
@@ -21,11 +31,63 @@ func (s *userLogInSuite) SetupTest() {
 	s.mockStorage = new(MockUserStorage)
 	s.usecase = NewUserLogIn(
 		s.mockStorage,
-		[]byte("jwt"),
+		[]byte(""),
 		time.Hour,
 		12*time.Hour,
 	)
 }
 
-func (s *userLogInSuite) TestUserLogIn() {
+func (s *userLogInSuite) TestUserLogInSuccess() {
+	s.mockStorage.GetUserCredsFunc = func(ctx context.Context, login string,
+	) (*storage.UserCreds, error) {
+		return &storage.UserCreds{
+			UserId:       uuid.New(),
+			PasswordHash: s.generatePasswordHash("password"),
+		}, nil
+	}
+
+	s.generatePrivateKey()
+
+	user, err := s.usecase.Do(context.Background(), "login", "password")
+	require.NoError(s.T(), err)
+
+	s.validateUuid(user.UserId)
+	require.Equal(s.T(), "login", user.Login)
+	s.validateToken(user.AccessToken)
+	s.validateToken(user.RefreshToken)
+}
+
+func (s *userLogInSuite) generatePrivateKey() {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(s.T(), err)
+
+	s.usecase.jwtSignKey = privateKey
+}
+
+func (s *userLogInSuite) generatePasswordHash(password string) string {
+	passwordBytes, err := bcrypt.GenerateFromPassword(
+		[]byte(password), bcrypt.DefaultCost)
+	require.NoError(s.T(), err)
+	return string(passwordBytes)
+}
+
+func (s *userLogInSuite) validateUuid(uuisString string) {
+	userUuid, err := uuid.Parse(uuisString)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), uuid.Version(0x04), userUuid.Version())
+}
+
+func (s *userLogInSuite) validateToken(tokenString string) {
+	keyFunc := func(t *jwt.Token) (any, error) {
+		if t.Method.Alg() != "ES256" {
+			return nil, fmt.Errorf("wrong algo")
+		}
+
+		// jwt.Parse invokes ECDSA parser, that expect *ecdsa.PublicKey
+		return &s.usecase.jwtSignKey.(*ecdsa.PrivateKey).PublicKey, nil
+	}
+
+	token, err := jwt.Parse(tokenString, keyFunc)
+	require.NoError(s.T(), err)
+	require.True(s.T(), token.Valid)
 }
--- microservices/auth/internal/usecases/user_register_test.go
+++ microservices/auth/internal/usecases/user_register_test.go
@@ -5,9 +5,11 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -33,7 +35,7 @@ func (s *userRegisterSuite) SetupTest() {
 	)
 }
 
-func (s *userRegisterSuite) TestUserLogInSuccess() {
+func (s *userRegisterSuite) TestUserRegisterSuccess() {
 	s.mockStorage.CreateUserFunc = func(
 		ctx context.Context, userId uuid.UUID, login string, passwordHash string,
 	) error {
@@ -45,10 +47,10 @@ func (s *userRegisterSuite) TestUserLogInSuccess() {
 	user, err := s.usecase.Do(context.Background(), "login", "password")
 	require.NoError(s.T(), err)
 
-	require.NotEmpty(s.T(), user.UserId)
+	s.validateUuid(user.UserId)
 	require.Equal(s.T(), "login", user.Login)
-	require.NotEmpty(s.T(), user.AccessToken)
-	require.NotEmpty(s.T(), user.RefreshToken)
+	s.validateToken(user.AccessToken)
+	s.validateToken(user.RefreshToken)
 }
 
 func (s *userRegisterSuite) generatePrivateKey() {
@@ -56,4 +58,25 @@ func (s *userRegisterSuite) generatePrivateKey() {
 	require.NoError(s.T(), err)
 
 	s.usecase.jwtSignKey = privateKey
+}
+
+func (s *userRegisterSuite) validateUuid(uuisString string) {
+	userUuid, err := uuid.Parse(uuisString)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), uuid.Version(0x04), userUuid.Version())
+}
+
+func (s *userRegisterSuite) validateToken(tokenString string) {
+	keyFunc := func(t *jwt.Token) (any, error) {
+		if t.Method.Alg() != "ES256" {
+			return nil, fmt.Errorf("wrong algo")
+		}
+
+		// jwt.Parse invokes ECDSA parser, that expect *ecdsa.PublicKey
+		return &s.usecase.jwtSignKey.(*ecdsa.PrivateKey).PublicKey, nil
+	}
+
+	token, err := jwt.Parse(tokenString, keyFunc)
+	require.NoError(s.T(), err)
+	require.True(s.T(), token.Valid)
 }
